52650639df29901c89e05f949e334c2a02a4b952
Jake Oehler Morrison <jmorrison@psesd.org>
2015-10-22 22:57:52 -0700

    unified menus

--- src/controllers/ProfileController.php
+++ src/controllers/ProfileController.php
@@ -9,9 +9,36 @@
 namespace canis\userManager\controllers;
 
 use Yii;
+use canis\web\unifiedMenu\Menu;
 
-class ProfileController extends BaseController
+class ProfileController 
+	extends BaseController
+	implements \canis\web\unifiedMenu\MenuProviderInterface
 {
+	public function behaviors()
+	{
+		return array_merge(parent::behaviors(), [
+			'UnifiedMenu' => [
+				'class' => \canis\web\unifiedMenu\ControllerBehavior::className()
+			]
+		]);
+	}
+
+    public static function provideMenuItems(Menu $menu)
+    {
+        $userManager = Yii::$app->getModule('userManager');
+        $menu = [];
+        $menu['profile'] = [
+            'url' => ['/'. $userManager->friendlyUrl .'/profile'],
+            'label' => 'Account'
+        ];
+        $menu['keys'] = [
+            'url' => ['/'. $userManager->friendlyUrl .'/profile/keys'],
+            'label' => 'Personal Access Tokens'
+        ];
+        return $menu;
+    }
+
 	public static function adminOnly()
     {
         return false;
@@ -20,6 +47,7 @@ class ProfileController extends BaseController
     public function actionIndex()
     {
         $userManager = Yii::$app->getModule('userManager');
+        $this->params['title'] = 'Account Profile';
         Yii::$app->response->view = 'index';
     }
 }
--- src/views/profile/index.php
+++ src/views/profile/index.php
@@ -1,1 +1,16 @@
-hi
+<?php
+use yii\helpers\Html;
+
+canis\web\assetBundles\BootstrapSelectAsset::register($this);
+$this->title = $title;
+echo Html::beginTag('div', ['class' => 'panel panel-default']);
+echo Html::beginTag('div', ['class' => 'panel-heading']);
+echo Html::beginTag('h3', ['class' => 'panel-title']);
+echo $this->title;
+echo Html::endTag('h3');
+echo Html::endTag('div');
+
+echo Html::beginTag('div', ['class' => 'panel-body']);
+echo "hi";
+echo Html::endTag('div');
+echo Html::endTag('div');
